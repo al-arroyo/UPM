35462ab2d888f7cadc8d2376a4dbd5f134245afc
al-arroyo <113766452+al-arroyo@users.noreply.github.com>
2023-03-17 17:03:54 +0100

    P2/esqueletos

---
+++ SEG/P2/esqueletos 2022/CA.java
@@ -0,0 +1,120 @@
+package p2;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.io.IOException;
+import java.math.BigInteger;
+
+import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.cert.X509v3CertificateBuilder;
+import org.bouncycastle.crypto.AsymmetricCipherKeyPair;
+import org.bouncycastle.pkcs.PKCS10CertificationRequest;
+import org.bouncycastle.pkcs.PKCSException;
+import java.util.Date;
+import org.bouncycastle.crypto.params.RSAKeyParameters;
+import org.bouncycastle.operator.ContentVerifierProvider;
+import org.bouncycastle.operator.DefaultDigestAlgorithmIdentifierFinder;
+import org.bouncycastle.operator.DefaultSignatureAlgorithmIdentifierFinder;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.bouncycastle.operator.bc.BcContentSignerBuilder;
+import org.bouncycastle.operator.bc.BcRSAContentSignerBuilder;
+import org.bouncycastle.operator.bc.BcRSAContentVerifierProviderBuilder;
+
+
+/**
+* Esta clase implementa el comportamiento de una CA
+* @author Seg Red Ser
+* @version 1.0
+*/
+public class CA {
+	
+	private final X500Name nombreEmisor;
+	private BigInteger numSerie;
+	private final int añosValidez; 
+	
+	public final static String NOMBRE_FICHERO_CRT = "CertificadoCA.crt";
+	public final static String NOMBRE_FICHERO_CLAVES = "CA-claves";
+	
+	private RSAKeyParameters clavePrivadaCA = null;
+	private RSAKeyParameters clavePublicaCA = null;
+	
+	/**
+	 * Constructor de la CA. 
+	 * Inicializa atributos de la CA a valores por defecto
+	 */
+	public CA () {
+		// Distinguished Name DN. C Country, O Organization name, CN Common Name. 
+		this.nombreEmisor = new X500Name ("C=ES, O=DTE, CN=CA");
+		this.numSerie = BigInteger.valueOf(1);
+		this.añosValidez = 1; // Son los años de validez del certificado de usuario, para la CA el valor es 4
+	}
+	
+	/**
+	* Método que genera la parejas de claves y el certificado autofirmado de la CA.
+	* @throws OperatorCreationException
+	* @throws IOException 
+	*/
+	public void generarClavesyCertificado() throws OperatorCreationException, IOException {
+		// Generar una pareja de claves (clase GestionClaves) y guardarlas EN FORMATO PEM en los ficheros 
+                // indicados por NOMBRE_FICHERO_CLAVES (añadiendo al nombre las cadenas "_pri.txt" y "_pu.txt")
+		// 
+		// Generar un certificado autofirmado: 
+		// 	1. Configurar parámetros para el certificado e instanciar objeto X509v3CertificateBuilder
+		// 	2. Configurar hash para resumen y algoritmo firma (MIRAR DIAPOSITIVAS DE APOYO EN MOODLE)
+		//	3. Generar certificado
+		//	4. Guardar el certificado en formato PEM como un fichero con extensión crt (NOMBRE_FICHERO_CRT)
+		//COMPLETAR POR EL ESTUDIANTE
+
+	
+	}
+
+
+
+
+	/**
+	 * Método que carga la parejas de claves
+	 * @throws IOException 
+	 */
+	public void cargarClaves () throws IOException{
+                // Carga la pareja de claves de los ficheros indicados por NOMBRE_FICHERO_CLAVES 
+                // (añadiendo al nombre las cadenas "_pri.txt" y "_pu.txt")
+		// No carga el certificado porque se lee de fichero cuando se necesita.
+		
+		GestionClaves gc = new GestionClaves(); // Clase con métodos para manejar las claves
+		//COMPLETAR POR EL ESTUDIANTE
+
+
+
+	}
+
+
+	
+	/**
+	 * Método que genera el certificado de un usuario a partir de una petición de certificación
+	 * @param ficheroPeticion:String. Parámetro con la petición de certificación
+	 * @param ficheroCertUsu:String. Parámetro con el nombre del fichero en el que se guardará el certificado del usuario
+	 * @throws IOException 
+	 * @throws PKCSException 
+	 * @throws OperatorCreationException
+	 */
+	public boolean certificarPeticion(String ficheroPeticion, String ficheroCertUsu) throws IOException, 
+	OperatorCreationException, PKCSException{
+		
+		//  Verificar que están generadas las clave privada y pública de la CA
+		//  Verificar firma del solicitante (KPSolicitante en fichero de petición) 
+		//  Si la verificación es ok, se genera el certificado firmado con la clave privada de la CA
+		//  Se guarda el certificado en formato PEM como un fichero con extensión crt
+
+		//  COMPLETAR POR EL ESTUDIANTE
+
+
+
+	
+	}
+	
+
+	// EL ESTUDIANTE PODRÁ CODIFICAR TANTOS MÉTODOS PRIVADOS COMO CONSIDERE INTERESANTE PARA UNA MEJOR ORGANIZACIÓN DEL CÓDIGO
---
+++ SEG/P2/esqueletos 2022/Usuario.java
@@ -0,0 +1,105 @@
+package p2;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Date;
+import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
+import org.bouncycastle.cert.CertException;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.crypto.AsymmetricCipherKeyPair;
+import org.bouncycastle.crypto.params.RSAKeyParameters;
+import org.bouncycastle.operator.ContentVerifierProvider;
+import org.bouncycastle.operator.DefaultDigestAlgorithmIdentifierFinder;
+import org.bouncycastle.operator.bc.BcRSAContentVerifierProviderBuilder;
+import org.bouncycastle.pkcs.PKCS10CertificationRequest;
+import org.bouncycastle.operator.OperatorCreationException;
+
+import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
+import org.bouncycastle.operator.DefaultSignatureAlgorithmIdentifierFinder;
+import org.bouncycastle.operator.bc.BcContentSignerBuilder;
+import org.bouncycastle.operator.bc.BcRSAContentSignerBuilder;
+import org.bouncycastle.pkcs.PKCS10CertificationRequestBuilder;
+import org.bouncycastle.pkcs.bc.BcPKCS10CertificationRequestBuilder;
+
+
+/**
+* Esta clase implementa el comportamiento de un usuario en una Infraestructura de Certificación
+* @author Seg Red Ser
+* @version 1.0
+*/
+public class Usuario {
+	
+	private RSAKeyParameters clavePrivada = null;
+	private RSAKeyParameters clavePublica = null;
+
+
+	/**
+	 * Método que genera las claves del usuario.
+	 * @param fichClavePrivada: String con el nombre del fichero donde se guardará la clave privada en formato PEM
+	 * @param fichClavePublica: String con el nombre del fichero donde se guardará la clave publica en formato PEM
+     * @throws IOException 	
+	
+	 */
+	public void generarClavesUsuario (String fichClavePrivada, String fichClavePublica) throws IOException{
+		
+		// Esto es nuevo respecto de la P1. Se debe instanciar un objeto de la clase GestionClaves proporcionada
+		// Tanto en Usuario como en CA
+		GestionClaves gc = new GestionClaves (); 
+		
+		// Asignar claves a los atributos correspondientes
+		// Escribir las claves en un fichero en formato PEM 
+
+		//IMPLEMENTAR POR EL ESTUDIANTE ESTUDIANTE
+		
+		
+    }
+
+
+
+	
+	/**
+	 * Método que genera una petición de certificado en formato PEM, almacenando esta petición en un fichero.
+	 * @param fichPeticion: String con el nombre del fichero donde se guardará la petición de certificado
+	 * @throws IOException 
+	 * @throws OperatorCreationException 
+	 */
+	public void crearPetCertificado(String fichPeticion) throws OperatorCreationException, IOException {
+		// IMPLEMENTAR POR EL ESTUDIANTE
+ 
+	   	// Configurar hash para resumen y algoritmo firma (MIRAR DIAPOSITIVAS PRESENTACIÓN PRÁCTICA)
+		// La solicitud se firma con la clave privada del usuario y se escribe en fichPeticion en formato PEM
+		
+
+
+		// IMPLEMENTAR POR EL ESTUDIANTE
+		
+	}
+	
+	
+	/**
+	 * Método que verifica un certificado de una entidad.
+	 * @param fichCertificadoCA: String con el nombre del fichero donde se encuentra el certificado de la CA
+	 * @param fichCertificadoUsu: String con el nombre del fichero donde se encuentra el certificado de la entidad
+     	 * @throws CertException 
+	 * @throws OperatorCreationException 
+	 * @throws IOException 
+	 * @throws FileNotFoundException 	
+	 * @return boolean: true si verificación OK, false en caso contrario.
+	 */
+    public boolean verificarCertificadoExterno(String fichCertificadoCA, String fichCertificadoUsu)throws OperatorCreationException, CertException, FileNotFoundException, IOException {
+
+    // IMPLEMENTAR POR EL ESTUDIANTE
+	// Comprobar fecha validez del certificado
+	// Si la fecha es válida, se comprueba la firma
+	// Generar un contenedor para la verificación con la clave pública de CA,
+	// el certificado del usuario tiene el resto de información
+    	
+   	// IMPLEMENTAR POR EL ESTUDIANTE
+  		
+	}	
+}
+
+	// EL ESTUDIANTE PODRÁ CODIFICAR TANTOS MÉTODOS PRIVADOS COMO CONSIDERE INTERESANTE PARA UNA MEJOR ORGANIZACIÓN DEL CÓDIGO
