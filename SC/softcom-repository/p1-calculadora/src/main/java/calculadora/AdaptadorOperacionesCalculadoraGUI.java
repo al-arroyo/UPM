063dd641d054d0dfcff8272a638a49c3a24ca9b8
al-arroyo <113766452+al-arroyo@users.noreply.github.com>
2023-09-30 21:12:15 +0200

    p1_fase1?

--- SC/softcom-repository/p1-calculadora/src/main/java/calculadora/AdaptadorOperacionesCalculadoraGUI.java
+++ SC/softcom-repository/p1-calculadora/src/main/java/calculadora/AdaptadorOperacionesCalculadoraGUI.java
@@ -11,60 +11,116 @@ public class AdaptadorOperacionesCalculadoraGUI implements ICalculadora {
 	// Escribir los métodos.
 	private	double	memoria = 0;
 	private	double	resultado = 0;
-	
+	private OperacionesCalculadora operaciones = new OperacionesCalculadora();
+	/*
+	 * default void memoriaAniadir()
+		Añade a la memoria acumuladora el último resultado obtenido al realizar una operación aritmética.
+	*/
 	@Override
-	public void memoriaAniadir() {
+	default void memoriaAniadir() {
 		// TODO Auto-generated method stub
-		this.memoria = obtenerUltimoResultado();
+		return operaciones.implementacionMA();
 	}
+	/*
+	 * default void memoriaLimpiar()
+		Pone a cero la memoria acumuladora de la calculadora.
+	 */
 	@Override
-	public void memoriaLimpiar() {
+	default void memoriaLimpiar() {
 		// TODO Auto-generated method stub
-		memoria = 0;
+		return operaciones.implementacionML();
 	}
+	/*
+	 * default double memoriaObtener()
+		Devuelve el valor almacenado en la memoria acumuladora de la calculadora.
+		Returns:
+			Valor de la memoria
+	 */
 	@Override
-	public double memoriaObtener() {
+	default double memoriaObtener() {
 		// TODO Auto-generated method stub
-		return memoria;
+		return operaciones.implementacionMO();
 	}
+	/*
+	 * default double obtenerUltimoResultado()
+		Devuelve el último resultado obtenido al realizar una operación aritmética. Si no se ha realizado ninguna operación aritmética antes, devuelve cero.
+		Returns:
+			último resultado obtenido al realizar una operación aritmética. Si no se ha realizado ninguna operación aritmética antes, devuelve cero.
+	 */
 	@Override
-	public double obtenerUltimoResultado() {
+	default double obtenerUltimoResultado() {
 		// TODO Auto-generated method stub
-		return resultado;
+		return operaciones.implementacionUR();
 	}
+	/*
+	 * default double multiplicar(double operando1, double operando2)
+		Multiplica los dos operandos y devuelve el resultado.
+		Parameters:
+			operando1 - Operando 1
+			operando2 - Operando 2
+		Returns:
+			Multiplicación de los dos operandos
+	 */
 	@Override
-	public double multiplicar(double operando1, double operando2) {
+	default double multiplicar(double operando1, double operando2) {
 		// TODO Auto-generated method stub
-		resultado = operando1*operando2;
-		return resultado;
+		return operaciones.implementacionMultiplicar(operando1, operando2);
 	}
+	/*
+	 * default double restar(double operando1, double operando2)
+		Resta el operando2 al operando1 y devuelve el resultado.
+		Parameters:
+			operando1 - Operando 1
+			operando2 - Operando 2
+		Returns:
+			Resta del operando2 al operando1
+	 */
 	@Override
-	public double restar(double operando1, double operando2) {
+	default double restar(double operando1, double operando2) {
 		// TODO Auto-generated method stub
-		resultado = (operando1-operando2);
-		return resultado;
+		return operaciones.implementacionRestar(operando1, operando2);
 	}
+	/*
+	 * default double sumar(double operando1, double operando2)
+		Suma los dos operandos y devuelve el resultado.
+		Parameters:
+			operando1 - Operando 1
+			operando2 - Operando 2
+		Returns:
+			Suma de los dos operandos
+	 */
 	@Override
-	public double sumar(double operando1, double operando2) {
+	default double sumar(double operando1, double operando2) {
 		// TODO Auto-generated method stub
-		resultado = (operando1+operando2);
-		return resultado;
+		return operaciones.implementacionSumar(operando1, operando2);
 	}
+	/*
+	 * default double dividir(double dividendo,	double divisor)throws Exception
+		Divide el dividendo por el divisor y devuelve el resultado.
+		Parameters:
+			dividendo - Operando dividendo
+			divisor - Operando divisor
+		Returns:
+			Division del dividendo por el divisor
+		Throws:
+			Exception - Excepción arrojada cuando se produce una división por cero o cuando se produce una indeterminación 0/0. La excepción debe contener un texto explicativo de la razón de la excepción.
+	 */
 	@Override
-	public double dividir(double dividendo, double divisor) throws Exception {
+	default double dividir(double dividendo, double divisor) throws Exception {
 		// TODO Auto-generated method stub
-		try {
-			resultado = (dividendo/divisor);
-		} catch (Exception e) {
-			// TODO: handle exception
-			System.out.println("El divisor no puede ser 0 o una indeterminacion 0/0");
-		}
-		return resultado;
+		return operaciones.implementacionDividir(dividendo, divisor);
 	}
+	/*
+	 * default double elevarAlCuadrado(double operando)
+		Eleva al cuadrado el operando y devuelve el resultado.
+		Parameters:
+			operando - Operando
+		Returns:
+			Elevado al cuadrado
+	 */
 	@Override
-	public double elevarAlCuadrado(double operando) {
+	default double elevarAlCuadrado(double operando) {
 		// TODO Auto-generated method stub
-		resultado = (operando*operando);
-		return resultado;
+		return operaciones.implementacionCuadrado(operando);
 	}
 }
--- SC/softcom-repository/p1-calculadora/src/main/java/calculadora/OperacionesCalculadora.java
+++ SC/softcom-repository/p1-calculadora/src/main/java/calculadora/OperacionesCalculadora.java
@@ -9,7 +9,45 @@ import CalculadoraGUI.ICalculadora;
  * la suma, su nombre en esta clase será 'implementacionSumar'.
  */
 public class OperacionesCalculadora{
-
+	private	double	memoria = 0;
+	private	double	resultado = 0;
 	// Escribir los métodos.
-
+	AdaptadorOperacionesCalculadoraGUI adaptador = new AdaptadorOperacionesCalculadoraGUI();
+	public double implementacionSumar(double operando1, double operando2){
+		resultado = (operando1+operando2);
+		return resultado;
+	}
+	public double implementacionRestar(double operando1, double operando2){
+				resultado = (operando1-operando2);
+		return resultado;
+	}
+	public double implementacionMultiplicar(double operando1, double operando2){
+		resultado = operando1*operando2;
+		return resultado;
+	}
+	public double implementacionDividir(double operando1, double operando2) throws Exception{
+		try {
+			resultado = (operando1/operando2);
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("El divisor no puede ser 0 o una indeterminacion 0/0");
+		}
+		return resultado;
+	}
+	public double implementacionCuadrado(double operando) throws Exception{
+		resultado = (operando*operando);
+		return resultado;
+	}
+	public double implementacionUR(){
+		return resultado;
+	}
+	public void implementacionMA(){
+		this.memoria += resultado;
+	}
+	public void implementacionML(){
+		memoria = 0;
+	}
+	public double implementacionMO(){
+		return memoria;
+	}
 }
